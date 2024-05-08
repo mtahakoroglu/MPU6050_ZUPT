d90c73f3ed10be68877e6996810e7cdccec0d2f7
Taha Koroglu <m.tahakoroglu@gmail.com>
2024-05-08 15:35:58 +0300

    EulerAngle.ino updated - pwm signal generation excluded

--- examples/EulerAngles/EulerAngles.ino
+++ examples/EulerAngles/EulerAngles.ino
@@ -1,14 +1,11 @@
 #include <MPU6050_ZUPT.h>
-#include <Servo.h>
 
-Servo motor;
 MPU6050 mpu6050(Wire);
 
 void setup() {
-  motor.attach(9);
   Serial.begin(57600);
   Wire.begin(100);
-  mpu6050.begin(200); // f = 200Hz
+  mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 }
 
@@ -17,8 +14,6 @@ void loop() {
   float roll = mpu6050.getAngleX();
   float pitch = mpu6050.getAngleY();
   float yaw = mpu6050.getAngleZ();
-  int pwm_signal = map(pitch, 0, 90, 1000, 2000);
-  motor.writeMicroseconds(pwm_signal);
   Serial.print("Roll: "); Serial.print(roll);
   Serial.print(" Pitch: "); Serial.print(pitch);
   Serial.print(" Yaw: "); Serial.println(yaw);
